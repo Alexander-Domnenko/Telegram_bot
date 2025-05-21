743dd60d53b05a584e37982ee6b7cefeac963180
Alexander-Domnenko <91257943+Alexander-Domnenko@users.noreply.github.com>
2025-05-21 21:18:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-# Токен бота из переменной окружения
-TOKEN =  '7709070463:AAGkDEXea7uYBFHt6ygelfp3M4LKoNvkojQ'
 
-# Список ID администраторов
-ADMIN_SECRET_CODE = "supersecret123"
-# URL базы данных из переменной окружения, с fallback на тестовую БД
-DB_URL = 'postgresql+asyncpg://testbot:shura8181@localhost:5432/testdb'
